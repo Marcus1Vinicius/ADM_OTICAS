c0f81347f531f0f5d0e819c5f4b02ebf1d43f1da
Marcus <omarcusvinicius96@gmail.com>
2025-05-31 17:18:27 -0500

    começando os trabalhos

---
+++ src/main/java/com/MV_dev/ADM_OTICA/AdmOticasApplication.java
@@ -0,0 +1,13 @@
+package com.MV_dev.ADM_OTICA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmOticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmOticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MV_dev/ADM_OTICA/AdmOticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MV_dev.ADM_OTICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmOticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
