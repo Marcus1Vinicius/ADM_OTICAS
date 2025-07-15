f014f751a8edc97c497d77ba5fcdf295c53f9c7f
Marcus <omarcusvinicius96@gmail.com>
2025-07-14 23:25:31 -0500

    criando o adm

--- src/main/java/com/MV_dev/ADM_OTICA/model/administrador_otica.java
+++ src/main/java/com/MV_dev/ADM_OTICA/model/administrador_otica.java
@@ -2,8 +2,19 @@ package com.MV_dev.ADM_OTICA.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.awt.image.BufferedImage;
+
 @Entity
 @Table
 public class administrador_otica {
+    private String nome;
+    private String celular;
+    private  String email;
+    private String senha;
+    private String CPF;
+    private String otica;
+    private BufferedImage imagem_perfil;
+
+
 
 }
