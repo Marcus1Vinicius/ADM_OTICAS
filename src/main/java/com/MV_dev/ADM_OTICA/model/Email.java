ae41323ba1ea5b9d37e57f96c8688a9941292a2c
Marcus <omarcusvinicius96@gmail.com>
2025-08-11 20:00:58 -0500

    começando com a classe email

--- src/main/java/com/MV_dev/ADM_OTICA/model/Email.java
+++ src/main/java/com/MV_dev/ADM_OTICA/model/Email.java
@@ -2,8 +2,19 @@ package com.MV_dev.ADM_OTICA.model;
 
 public class Email {
     private String Email;
+    // Construtor privado para evitar a criação de instâncias sem validação
+    private Email(String email) {
+        this.Email = email;
+    }
 
-    public static boolean EmailValido(String Email){
+    public String getEmail() {
+        return this.Email;
+    }
 
+    public static boolean EmailValido(String Email) {
+
+
+
+        return false;
     }
 }
